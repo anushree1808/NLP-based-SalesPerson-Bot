4d2c6701b2719531fdc9d07d731af185ed5dcb13
anushree1808 <kranush93@gmail.com>
2016-09-21 23:18:33 -0700

    Update feature_functions.py

--- feature_functions.py
+++ feature_functions.py
@@ -1,12 +1,3 @@
-'''
-feature_functions.py
-Implements the feature generation mechanism
-Author: Anantharaman Narayana Iyer
-Date: 21 Nov 2014
-
-6th Dec: Org gazeteer added
-7th Dec: 
-'''
 from nltk import sent_tokenize, word_tokenize
 import nltk
 import numpy
